007ccbeaa0558a5a7b9a1c0916989ec2008077ce
gevarghese <kvgevarghese@gmail.com>
2025-09-08 09:05:44 +0530

    fix message issue of device

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -12,11 +12,19 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
 
-  # protected
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
+    end
+  end
+
+  protected
 
   # The path used after resending confirmation instructions.
   # def after_resending_confirmation_instructions_path_for(resource_name)
@@ -24,7 +32,11 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+  def after_confirmation_path_for(resource_name, resource)
+    if signed_in?(resource_name)
+      signed_in_root_path(resource)
+    else
+      new_session_path(resource_name)
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,18 @@
 module ApplicationHelper
   include BlogsHelper
   include UiHelper
-  
+
   def admin?
     current_user&.admin?
   end
-  
+
   def moderator?
     current_user&.moderator?
+  end
+
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    render "devise/shared/error_messages", resource: resource
   end
 end
